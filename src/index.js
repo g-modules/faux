33f69660a4e4a83f0b17b20f35c1c78fe0ac78d0
Constantine Gololobov <dev@gololobov.dev>
2021-08-10 11:42:59 +0200

    Test Fixes + Editor Config

--- src/index.js
+++ src/index.js
@@ -1,38 +1,4 @@
-// import { randomNumber, randomBool, randomId, capitalizeFirstLetter, shuffleArray, uniqObjectsInArray } from "./helpers/utils.js";
+/* eslint no-empty-function: 0 */
 export default function faux () {
-	// const minimum = 0;
-	// const maximum = 1;
-	// const randomNumberValue = randomNumber( minimum, maximum );
-	// console.log(`randomNumber: ${randomNumberValue}`);
 
-	// const chance = 10;
-	// const randomBoolValue = randomBool( chance );
-	// console.log(`randomTrue: ${randomBoolValue}`);
-
-	// const randomIdValue = randomId()
-	// console.log(randomIdValue)
-
-	// const originalString = 4;
-	// const upperCasedString = capitalizeFirstLetter( originalString );
-	// console.log( `${originalString} => ${upperCasedString}` );
-
-	// const originalArray = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, {hello: "world"}, ["a", "b", "c"], undefined, null ]
-	// const shuffledArray = shuffleArray(originalArray)
-	// console.log(shuffledArray)
-
-
-	// const array = [{
-	// 	"id": 1,
-	// 	"text": "hi"
-	// }, {
-	// 	"id": 2,
-	// 	"text": "hello"
-	// }, {
-	// 	"id": 3,
-	// 	"text": "ahoj"
-	// }, {
-	// 	"id": 4,
-	// 	"text": "hi"
-	// }];
-	// const uniq = uniqObjectsInArray( array, "id" );
 }
--- tests/unit/helpers/utils.test.js
+++ tests/unit/helpers/utils.test.js
@@ -1,11 +1,17 @@
-/* global describe, test, expect */
+/* global describe, beforeEach, test, expect */
 import { randomNumber, randomBool, randomId, capitalizeFirstLetter, shuffleArray, uniqObjectsInArray } from "../../../src/helpers/utils.js";
 
 
 describe( "randomNumber():", () => {
-	const minimum = -10;
-	const maximum = 10;
-	const result = randomNumber( minimum, maximum );
+	let minimum;
+	let maximum;
+	let result;
+	beforeEach( () => {
+		minimum = -10;
+		maximum = 10;
+		result = randomNumber( minimum, maximum );
+	});
+
 	test( "should return a number", () => expect( result ).toBeNumber() );
 	test( "should return a number, without passing params", () => expect( randomNumber() ).toBeNumber() );
 	test( "should return a value between minimum and maximum numbers, inclusive them", () => {
@@ -29,8 +35,13 @@ describe( "randomId():", () => {
 });
 
 describe( "capitalizeFirstLetter():", () => {
-	const originalString = "hello world!";
-	const result = capitalizeFirstLetter( originalString );
+	let originalString;
+	let result;
+	beforeEach( () => {
+		originalString = "hello world!";
+		result = capitalizeFirstLetter( originalString );
+	});
+
 	test( "should return a string", () => expect( result ).toBeString() );
 	test( "shouldn't return an empty string", () => expect( result ).not.toBeEmpty() );
 	test( "shouldn't return unchanged string", () => expect( result ).not.toBe( originalString ) );
@@ -47,8 +58,13 @@ describe( "capitalizeFirstLetter():", () => {
 });
 
 describe( "shuffleArray():", () => {
-	const originalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, { "hello": "world" }, ["a", "b", "c"], undefined, null];
-	const result = shuffleArray( originalArray );
+	let originalArray;
+	let result;
+	beforeEach( () => {
+		originalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, { "hello": "world" }, ["a", "b", "c"], undefined, null];
+		result = shuffleArray( originalArray );
+	});
+
 	test( "should return an array", () => expect( result ).toBeArray() );
 	test( "should return an array of same size", () => expect( result ).toBeArrayOfSize( originalArray.length ) );
 	test( "should return an array that contain all original values", () => expect( result ).toIncludeAllMembers( originalArray ) );
@@ -56,17 +72,22 @@ describe( "shuffleArray():", () => {
 });
 
 describe( "uniqObjectsInArray():", () => {
-	const originalArray = [{
-		"id": 1,
-		"text": "hi"
-	}, {
-		"id": 2,
-		"text": "hello"
-	}, {
-		"id": 3,
-		"text": "hi"
-	}];
-	const result = uniqObjectsInArray( originalArray, "text" );
+	let originalArray;
+	let result;
+	beforeEach( () => {
+		originalArray = [{
+			"id": 1,
+			"text": "hi"
+		}, {
+			"id": 2,
+			"text": "hello"
+		}, {
+			"id": 3,
+			"text": "hi"
+		}];
+		result = uniqObjectsInArray( originalArray, "text" );
+	});
+
 	test( "should return an array", () => expect( result ).toBeArray() );
 	test( "should return an array that contain any original objects", () => expect( result ).toIncludeAnyMembers( originalArray ) );
 	test( "should return same array, if value uniq", () => expect( uniqObjectsInArray( originalArray, "id" ) ).toEqual( originalArray ) );
